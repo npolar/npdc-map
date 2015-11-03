f471b82c80dce3f28fb84f5f9f7458af1c07e3b6
RDuX <ruben.dens@gmail.com>
2015-11-03 16:23:04 +0100

    refactored search

--- src/map-archive/search/MapArchiveSearchController.js
+++ src/map-archive/search/MapArchiveSearchController.js
@@ -2,42 +2,43 @@
 /**
  * @ngInject
  */
-var MapArchiveSearchController = function ($scope,  $controller, $location, $log, MapArchive, MapImageService, NpdcAutocompleteConfig) {
+var MapArchiveSearchController = function ($scope,  $controller, $location, $log, MapArchive, MapImageService, npdcAppConfig) {
 
   $controller('NpolarEditController', { $scope: $scope });
-  
+
   $scope.resource = MapArchive;
   $scope.img = MapImageService;
-    
-  let defaults = { limit: 25, sort: "-updated", fields: 'id,publication.code,title,subtitle,links,publication.year,collection',
+
+  let defaults = { limit: 25, sort: "-updated", fields: 'id,publication.code,title,subtitle,links,publication.year,collection,location.area',
     facets: 'type,location.area,location.country',
     'rangefacet-publication.year': 25
   };
-  
+
   let invariants = $scope.security.isAuthenticated() ? {} : { 'not-restricted': true, 'filter-links.rel': 'edit-media'
     //,'filter-links.length': '0..'
   };
-           
+
   if ($scope.security.isAuthenticated() && $scope.security.isAuthorized('read', MapArchive.path))   {
 
     defaults.facets += ',archives.restricted,organisation,publication.publisher,publication.code,publication.series,publication.country,location.hemisphere';
-    
+
     if (!$location.search()['filter-created']) {
       defaults['date-year'] = 'created';
     } else {
       defaults['date-month'] = 'created';
     }
   }
-        
+
   let query = Object.assign(defaults, $location.search(), invariants);
-  
+
   $log.debug(query);
-  
-  NpdcAutocompleteConfig.selectedDefault = ['map/archive'];
-  NpdcAutocompleteConfig.placeholder = 'Search map archive';
-  NpdcAutocompleteConfig.query = defaults;
-  
   $scope.search(query);
+
+  npdcAppConfig.cardTitle = "Map Archive";
+  npdcAppConfig.search.results = {
+    subtitle: "location/area",
+    detail: "publication/year"
+  };
 
 };
 
