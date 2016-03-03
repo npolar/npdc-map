f71cbbf0db2bc369d169367bca9284d1f3551a42
cnrdh <cnrdh@users.noreply.github.com>
2016-03-03 11:57:10 +0100

    Added file upload

--- src/map-archive/edit/MapArchiveEditController.js
+++ src/map-archive/edit/MapArchiveEditController.js
@@ -1,14 +1,14 @@
 'use strict';
-// @todo
+// FIXME new => set processor, archives etc.
+
 // authors/editors
-// rightsHolder
-// licensing / metadata
-// languages
-// remove placename
-// citation?
-// => countries /locations
-// code => sheet
-//// show => hemisphere
+//"homepage": {
+//  "type": "string",
+//  "description": "Web address of rights holder"
+//},
+//"
+
+// code => sheet ?
 //
 //ISO 19115	/ 19115-1 roles
 //+ resourceProvider
@@ -35,54 +35,85 @@
 /**
  * @ngInject
  */
-var MapArchiveEditController = function ($scope, $controller, $http, $log, $routeParams, $window, NpolarApiSecurity, MapArchive, MapImageService) {
-    
-  // Extend -> NpolarEditController
+let MapArchiveEditController = function ($scope, $controller, $http, $log, $routeParams, $window,
+  formula, formulaAutoCompleteService, fileFunnelService, chronopicService,                            
+  npolarApiConfig, npdcAppConfig, NpdcSearchService, NpolarApiSecurity, NpolarLang, 
+  MapArchive, MapImageService) {
+  
+  const schema = '//api.npolar.no/schema/map-archive-1';
+  
   $controller('NpolarEditController', { $scope: $scope });
-
-  // MapArchive -> npolarApiResource -> ngResource
   $scope.resource = MapArchive;
-  
   $scope.img = MapImageService;
-  
-  let user = $scope.security.getUser();
-  
-  let processor = {
-    email: user.email,
-    name: user.name
-  };
 
-  // Formula ($scope.formula is set by parent)
-  $scope.formula.schema = `//${MapImageService.schema.split('//')[1]}`;
-  console.debug(MapImageService.schema);
-  $scope.formula.template = 'material';
-  $scope.formula.form = 'map-archive/edit/map-archive-formula.json';
+  $scope.formula = formula.getInstance({
+    schema,
+    form: 'map-archive/edit/map-archive-formula.json',
+    templates: npdcAppConfig.formula.templates,
+    languages: npdcAppConfig.formula.languages
+  });
   
+  let autocompleteFacets = ['publishers.name', 'rightsHolder.name', 'archives.organisation', 'publication.country'];
+  formulaAutoCompleteService.autocompleteFacets(autocompleteFacets, $scope.resource, $scope.formula);
   
-  // Edit action, ie. fetch document and edit with formula
-  // @override
-  $scope.editAction = function() {
+  fileFunnelService.fileUploader({
+    match(field) {
+      return field.id === "images";
+    },
+    server: `${NpolarApiSecurity.canonicalUri(npolarApiConfig.base)}/map/archive/${id}/_file`,
+    multiple: true,
+    progress: false,
+    //restricted: (map) => map.restricted,
+    successCallback: MapImageService.editMediaLinkFromFileUpload
+  }, $scope.formula);
     
-    $scope.resource.fetch($routeParams, function(document) {
-      
-      // Automatically add link to TIFF (link.rel = edit-media)
-      if (!document.links) {
-        document.links = [MapImageService.editMediaLink(document)];
-      }     
-      $scope.document = document;
-      $scope.formula.model = document;
-      
-    });
-  };
 
+  //console.log(`${NpolarApiSecurity.canonicalUri(npolarApiConfig.base)}/map/archive/${id}/_file/`);
+  //console.debug(NpolarApiSecurity.canonicalUri());
+  /*$scope.formula.templates = [
+    {
+      match(field) {
+        //console.log(field.path);
+        return field.id === "placenames_object"; },
+      template: '<npdc:formula-placename></npdc:formula-placename>'
+    }
+  ];*/
+  //chronopicService.defineOptions('#/rightsExpire', {locale: NpolarLang.getLang(), format: '{YYYY}-{MM}-{DD}'});
+  
   // New action, ie. create new document and edit with formula
   // @override
   $scope.newAction = function() {
-    $scope.document = MapImageService.newMap(processor);
+    // Set "processor" to current user
+    let user = $scope.security.getUser();
+    let processor = {
+      email: user.email,
+      name: user.name
+    };
+    let contributors = [{ name: user.name, role: 'editor', email: user.email }];
+    let type = 'topographic';
+    let collection = 'map-archive';
+    let title = `New map created by ${user.name}`; 
+    let archives = [
+      {
+        'placename': 'Tromsø',
+        'where': '5083',
+        'count': 1,
+        'country': 'NO',
+        'organisation': 'Norsk Polarinstitutt'
+      }
+    ];
+    $scope.document = { title, type, collection, schema, archives, processor, contributors };
     $scope.formula.model = $scope.document;
   };
   
-  $scope.edit();
+  let r = $scope.edit(true /* generate uuid */);
+  if (r && r.$promise) {
+    r.$promise.then(map => {
+      
+
+
+    });
+  }
 
 };
 
