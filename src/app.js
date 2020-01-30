a5a190c13f04626679dc00da2456c73419400099
cnrdh <conrad@npolar.no>
2020-01-30 10:38:17 +0100

    Fix jpeg images sometimes not showing

--- src/app.js
+++ src/app.js
@@ -1,63 +1,90 @@
-'use strict';
+"use strict";
 
-var npdcCommon = require('npdc-common');
+var npdcCommon = require("npdc-common");
 var AutoConfig = npdcCommon.AutoConfig;
-var angular = require('angular');
-require('npdc-common/src/wrappers/leaflet');
+var angular = require("angular");
+require("npdc-common/src/wrappers/leaflet");
 
-
-var npdcMapApp = angular.module('npdcMapArchiveApp', ['npdcCommon', 'leaflet']);
+var npdcMapApp = angular.module("npdcMapArchiveApp", ["npdcCommon", "leaflet"]);
 
 /*npdcMapApp.factory('L', function() {
   return window.L; // assumes Leaflet has already been loaded on the page
 });*/
 
-npdcMapApp.service('MapImageService', require('./map-archive/image/MapImageService'));
-npdcMapApp.factory('MapArchive', require('./map-archive/edit/MapArchive'));
-npdcMapApp.controller('MapArchiveShowController', require('./map-archive/show/MapArchiveShowController'));
-npdcMapApp.controller('MapArchiveSearchController', require('./map-archive/search/MapArchiveSearchController'));
-npdcMapApp.controller('MapArchiveEditController', require('./map-archive/edit/MapArchiveEditController'));
-npdcMapApp.directive('mapCoverage', require('./map-archive/edit/coverage/coverageDirective'));
-
+npdcMapApp.service(
+  "MapImageService",
+  require("./map-archive/image/MapImageService")
+);
+npdcMapApp.factory("MapArchive", require("./map-archive/edit/MapArchive"));
+npdcMapApp.controller(
+  "MapArchiveShowController",
+  require("./map-archive/show/MapArchiveShowController")
+);
+npdcMapApp.controller(
+  "MapArchiveSearchController",
+  require("./map-archive/search/MapArchiveSearchController")
+);
+npdcMapApp.controller(
+  "MapArchiveEditController",
+  require("./map-archive/edit/MapArchiveEditController")
+);
+npdcMapApp.directive(
+  "mapCoverage",
+  require("./map-archive/edit/coverage/coverageDirective")
+);
 
 // Bootstrap ngResource models using NpolarApiResource
 var resources = [
-  {'path': '/map/archive', 'resource': 'MapArchiveResource'},
+  {
+    path: "/map/archive",
+    resource: "MapArchiveResource",
+    base: "https://api.npolar.no"
+  }
 ];
 
 resources.forEach(service => {
   // Expressive DI syntax is needed here
-  npdcMapApp.factory(service.resource, ['NpolarApiResource', function (NpolarApiResource) {
-    return NpolarApiResource.resource(service);
-  }]);
+  npdcMapApp.factory(service.resource, [
+    "NpolarApiResource",
+    function(NpolarApiResource) {
+      return NpolarApiResource.resource(service);
+    }
+  ]);
 });
 
 // Routes
-npdcMapApp.config(require('./routes'));
+npdcMapApp.config(require("./routes"));
 
 // API HTTP interceptor
 npdcMapApp.config($httpProvider => {
-  $httpProvider.interceptors.push('npolarApiInterceptor');
+  $httpProvider.interceptors.push("npolarApiInterceptor");
 });
 
 // Inject npolarApiConfig and run
-npdcMapApp.run(($http, npolarApiConfig, npdcAppConfig, NpolarTranslate, NpolarLang) => {
+npdcMapApp.run(
+  ($http, npolarApiConfig, npdcAppConfig, NpolarTranslate, NpolarLang) => {
+    var environment = "production";
+    var autoconfig = new AutoConfig(environment);
 
-  var environment = "production";
-  var autoconfig = new AutoConfig(environment);
+    Object.assign(npolarApiConfig, autoconfig, {
+      resources,
+      formula: { template: "default" }
+    });
+    console.log("npolarApiConfig", npolarApiConfig);
 
-  Object.assign(npolarApiConfig, autoconfig, { resources, formula : { template : 'default' } });
-  console.log("npolarApiConfig", npolarApiConfig);
+    // i18n
+    $http
+      .get(
+        "https://api.npolar.no/text/?q=&filter-bundle=npdc-map&format=json&variant=array&limit=all"
+      )
+      .then(response => {
+        NpolarTranslate.appendToDictionary(response.data);
 
-  // i18n
-  $http.get('//api.npolar.no/text/?q=&filter-bundle=npdc-map&format=json&variant=array&limit=all').then(response => {
-
-    NpolarTranslate.appendToDictionary(response.data);
-
-    NpolarTranslate.dictionary['npdc.app.Title'] = [
-      {'@language': 'en', '@value': 'Maps'},
-      {'@language': 'no', '@value': 'Kart'}
-    ];
-    NpolarLang.setLanguages(npdcAppConfig.i18n.languages);
-  });
-});
+        NpolarTranslate.dictionary["npdc.app.Title"] = [
+          { "@language": "en", "@value": "Maps" },
+          { "@language": "no", "@value": "Kart" }
+        ];
+        NpolarLang.setLanguages(npdcAppConfig.i18n.languages);
+      });
+  }
+);
--- src/map-archive/show/MapArchiveShowController.js
+++ src/map-archive/show/MapArchiveShowController.js
@@ -1,23 +1,29 @@
-'use strict';
+"use strict";
 
-function MapArchiveShowController($scope, $controller, $routeParams, $timeout,
-  NpolarApiSecurity, MapArchive, MapImageService) {
+function MapArchiveShowController(
+  $scope,
+  $controller,
+  $routeParams,
+  $timeout,
+  NpolarApiSecurity,
+  MapArchive,
+  MapImageService
+) {
+  "ngInject";
 
-  'ngInject';
+  const npolar = "Norsk Polarinstitutt";
 
-  const npolar = 'Norsk Polarinstitutt';
-
-  $controller('MapArchiveSearchController', {$scope: $scope});
+  $controller("MapArchiveSearchController", { $scope: $scope });
 
   this.qualities = [
-    { value: 'high', text: 'High quality' },
-    { value: 'web', text: 'Web quality' }
+    { value: "high", text: "High quality" },
+    { value: "web", text: "Web quality" }
   ];
-  this.quality = 'high';
+  this.quality = "high";
 
   $scope.total_size = () => {
     let sum = 0;
-    $scope.images.forEach(i => sum += i.length);
+    $scope.images.forEach(i => (sum += i.length));
     return sum;
   };
 
@@ -34,81 +40,89 @@ function MapArchiveShowController($scope, $controller, $routeParams, $timeout,
 
   function attributionNames(map) {
     let names = [];
-    let rightsHolders = (map.contributors||[]).filter(c => { return (c.role === 'rightsHolder' && c.name); });
+    let rightsHolders = (map.contributors || []).filter(c => {
+      return c.role === "rightsHolder" && c.name;
+    });
     if (rightsHolders.length > 0) {
       names = rightsHolders.map(r => r.name);
     } else if (map.publishers) {
-      names = (map.publishers||[]).map(p => p.name);
+      names = (map.publishers || []).map(p => p.name);
     }
     if (names.length === 0) {
       names = npolar;
     }
-    console.log('attributionNames', names, 'map', map);
+    console.log("attributionNames", names, "map", map);
     return names;
   }
 
   let show = function() {
-    MapArchive.fetch($routeParams, (map) => {
-
+    MapArchive.fetch($routeParams, map => {
       $scope.document = map;
       $scope.attributionNames = attributionNames(map);
       $scope.document.organisations = map.contributors;
 
       // Set images, prefer PNGs
-      const png = map.files.filter(f => (/^image\/png/).test(f.type));
-      const tiff = map.files.filter(f => (/^image\/tiff/).test(f.type));
-      if (png) {
+
+      const png = map.files.filter(f => /^image\/png/.test(f.type));
+      const tiff = map.files.filter(f => /^image\/tiff/.test(f.type));
+
+      if (png && png.length > 0) {
         $scope.images = png;
-      } else if (tiff) {
+      } else if (tiff && tiff.length > 0) {
         $scope.images = tiff;
       } else {
-        $scope.images = map.files.filter(f => (/^image\//).test(f.type));
+        $scope.images = map.files.filter(f => /^image\//.test(f.type));
       }
 
       if (map.geometry && map.geometry.bbox.length === 4) {
         let bbox = map.geometry.bbox;
-        let bounds = [ [bbox[1], bbox[0]], [bbox[3], bbox[2]] ];
+        let bounds = [[bbox[1], bbox[0]], [bbox[3], bbox[2]]];
 
         $scope.mapOptions.bbox = map.geometry.bbox;
-        $scope.mapOptions.images = [MapImageService.icon($scope.images[0], map)];
+        $scope.mapOptions.images = [
+          MapImageService.icon($scope.images[0], map)
+        ];
         $scope.mapOptions.coverage = [bounds];
         //$scope.mapOptions.geojson = "geojson";
       }
 
-
       // todo f(x)
-      let q = map.title +' '+ map.title +' '+ map.code;
-      let relatedQuery = { q , fields: 'id,code,title,type,publication.year,restricted', score: true, limit: "all" };
-      let notSelfQuery = {'not-id': map.id };
+      let q = map.title + " " + map.title + " " + map.code;
+      let relatedQuery = {
+        q,
+        fields: "id,code,title,type,publication.year,restricted",
+        score: true,
+        limit: "all"
+      };
+      let notSelfQuery = { "not-id": map.id };
 
       MapArchive.array(Object.assign(relatedQuery, notSelfQuery), related => {
         $scope.similar.maps = related.filter(r => r._score > 0.4);
       });
-
     });
   };
 
   $scope.key = function() {
-    let system = NpolarApiSecurity.getSystem('read', MapArchive.path);
+    let system = NpolarApiSecurity.getSystem("read", MapArchive.path);
     if (system.key) {
       return system.key;
     }
   };
 
-  $scope.scale = function (scale) {
-    return scale > 0 ? '1:'+scale : '';
+  $scope.scale = function(scale) {
+    return scale > 0 ? "1:" + scale : "";
   };
 
-  $scope.year = function (year) {
-    return year > 0 ? year : 'unknown year';
+  $scope.year = function(year) {
+    return year > 0 ? year : "unknown year";
   };
 
-  $scope.principalInvestigators = function(contributors=[]) {
-    return contributors.filter(c => c.role === 'principalInvestigator');
+  $scope.principalInvestigators = function(contributors = []) {
+    return contributors.filter(c => c.role === "principalInvestigator");
   };
 
-  $scope.editors = function(contributors=[]) {
-    return contributors.filter(c => c.role === 'editor');
+  $scope.editors = function(contributors = []) {
+    return contributors.filter(c => c.role === "editor");
   };
 
   $scope.language = function(code) {
@@ -116,11 +130,10 @@ function MapArchiveShowController($scope, $controller, $routeParams, $timeout,
   };
 
   init();
-      // Wrap Leaflet map in a $timeout
-    $timeout(() => {
-      show();
-    }); // end $timeou
-
+  // Wrap Leaflet map in a $timeout
+  $timeout(() => {
+    show();
+  }); // end $timeou
 }
 
 module.exports = MapArchiveShowController;
